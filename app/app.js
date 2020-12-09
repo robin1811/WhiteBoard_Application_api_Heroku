165f96db4a273afeadf38be8a87d4a5b6e472e7f
kunwar <singh.dhadial@gmail.com>
2020-12-09 10:00:48 +0530

    initial commit

---
+++ app/app.js
@@ -0,0 +1,50 @@
+// remove nodemon from nodemon app.js from json file before deployment on heroku.
+const express = require("express");
+const app = express();
+
+const http = require("http").createServer(app);
+
+const io = require("socket.io")(http , {cors: {
+    origin: '*',
+  }});
+
+io.on("connection", function(socket){
+    console.log( `${socket.id} user connected`);
+
+    socket.on("mousedown", function(data){
+        socket.broadcast.emit("md",data);
+    })
+    socket.on("mousemove", function(data){
+        socket.broadcast.emit("mm",data);
+    })
+    socket.on("bgColor",function(data){
+        socket.broadcast.emit("myBackgroundColor", data);
+    })
+
+}) 
+
+app.get("/" , function(req, res){
+    res.write("<h1> Welcome to home page !!! </h1>");
+    res.send();
+});
+     
+    // socket.on("testing", function(data){
+    //     console.log(data);
+    //     // socket.emit("testing-again",data);
+    // });
+
+
+//  app.get("/profile" ,function(req, res){
+//     res.send("<h1> Welcome to profile page !!! </h1>");
+//  })
+
+let port = process.env.PORT || 3000;
+
+http.listen(port , function(){
+     console.log("App is listening to 3000 !!!");
+ })
+
+// app.listen(3000 , function(){
+//     console.log("App is listening to 3000 !!!");
+// })
+
